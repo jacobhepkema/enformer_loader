7562480e8a42c01d6a579965b5d5a5a611b0ceaf
Jacob Hepkema <jacobhepkema@gmail.com>
2024-06-11 15:30:19 +0100

    fix typo in utils.py

--- enformer_loader/utils.py
+++ enformer_loader/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Many functions adapted from Adapted from
+# Many functions adapted from
 # https://github.com/wconnell/enformer-finetune/blob/c2145a628efcb91b932cc063a658e4a994bc4baa/eft/preprocess.py
 
 import numpy as np
